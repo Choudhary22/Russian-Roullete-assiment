8403552bf5eab484d768fb867e4ae05f87205f9d
anil kumar <anilmalgash@gmail.com>
2019-05-17 11:19:21 +1200

    correct upload

---
+++ Russian Roullete assiment/game.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Russian_Roullete_assiment
+{
+    partial class game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(game));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_spin = new System.Windows.Forms.Button();
+            this.btn_Shoot = new System.Windows.Forms.Button();
+            this.btn_retry = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Azure;
+            this.label1.Location = new System.Drawing.Point(103, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Shooting Game";
+            // 
+            // btn_load
+            // 
+            this.btn_load.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_load.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_load.Location = new System.Drawing.Point(401, 131);
+            this.btn_load.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(96, 74);
+            this.btn_load.TabIndex = 1;
+            this.btn_load.Text = "load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_spin
+            // 
+            this.btn_spin.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_spin.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_spin.Location = new System.Drawing.Point(401, 224);
+            this.btn_spin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btn_spin.Name = "btn_spin";
+            this.btn_spin.Size = new System.Drawing.Size(96, 70);
+            this.btn_spin.TabIndex = 2;
+            this.btn_spin.Text = "Spin";
+            this.btn_spin.UseVisualStyleBackColor = true;
+            this.btn_spin.Click += new System.EventHandler(this.btn_spin_Click);
+            // 
+            // btn_Shoot
+            // 
+            this.btn_Shoot.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Shoot.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_Shoot.Location = new System.Drawing.Point(507, 131);
+            this.btn_Shoot.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btn_Shoot.Name = "btn_Shoot";
+            this.btn_Shoot.Size = new System.Drawing.Size(95, 74);
+            this.btn_Shoot.TabIndex = 3;
+            this.btn_Shoot.Text = "Shoot";
+            this.btn_Shoot.UseVisualStyleBackColor = true;
+            this.btn_Shoot.Click += new System.EventHandler(this.btn_Shoot_Click);
+            // 
+            // btn_retry
+            // 
+            this.btn_retry.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_retry.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_retry.Location = new System.Drawing.Point(510, 228);
+            this.btn_retry.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btn_retry.Name = "btn_retry";
+            this.btn_retry.Size = new System.Drawing.Size(92, 66);
+            this.btn_retry.TabIndex = 4;
+            this.btn_retry.Text = "Retry";
+            this.btn_retry.UseVisualStyleBackColor = true;
+            this.btn_retry.Click += new System.EventHandler(this.btn_retry_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 103);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 253);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(666, 433);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_retry);
+            this.Controls.Add(this.btn_Shoot);
+            this.Controls.Add(this.btn_spin);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Mistral", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "game";
+            this.Text = "game";
+            this.Load += new System.EventHandler(this.game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_spin;
+        private System.Windows.Forms.Button btn_Shoot;
+        private System.Windows.Forms.Button btn_retry;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
